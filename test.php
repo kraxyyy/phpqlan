39c20c463b5ee7916ba466a132dc2c0ce3fe7399
Lukas Erhart <16212678+kraxyyy@users.noreply.github.com>
2019-02-19 13:36:18 +0100

    Added test and example file for other developers to have a clue how it's working

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+
+spl_autoload_register(function ($class_name) {
+    $directories = ["database/", "infrastructure/", "infrastructure/attributes", "infrastructure/sources"];
+
+    foreach ($directories as $dir) {
+        if(file_exists($dir . $class_name . '.php')) {
+            include_once $dir . $class_name . '.php';
+            return true;
+        }
+    }
+    return false;
+});
+
+$array = [
+    ["name" => "Max Mustermann", "age" => 19],
+    ["name" => "Maximiliane Mustermann", "age" => 23],
+    ["name" => "Markus Mustermann", "age" => 21]
+];
+
+$list = new QueryList($array);
+
+$list->setToStringFunction(function($item) {
+
+    echo 'Person: ' . $item["name"] . '<br>';
+
+})->toString()->where(function($item) {
+
+   return $item["age"] > 20;
+
+})->take(2)->shuffle()->display(function($item) {
+
+    return $item["name"] . ' > ' . $item["age"] . '<br>';
+
+});
